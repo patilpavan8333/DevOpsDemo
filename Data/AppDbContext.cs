eea4cb7ac9c567d919cf393d8956897fd5b957d2
pavan <pavanpatil833@gamil.com>
2026-09-22 16:12:30 +0530

    add all files

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using DevOpsDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevOpsDemo.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products => Set<Product>();
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+using DevOpsDemo.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
